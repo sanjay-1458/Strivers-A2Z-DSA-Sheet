8ea093b97846005858df12bef6b66af85a10415f
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-09-24 12:22:00 +0530

    Update Implement Lower Bound.cpp

--- Implement Lower Bound.cpp
+++ Implement Lower Bound.cpp
@@ -1,3 +1,6 @@
+// Lower Bound = arr[i] >= x
+// Lower Bound = lower_bound(arr.begin(),arr.end(),x) - arr.begin()
+
 int lowerBound(vector<int> arr, int n, int target) {
 	// Write your code here
 	int low=0,high=n-1,val=-1;
