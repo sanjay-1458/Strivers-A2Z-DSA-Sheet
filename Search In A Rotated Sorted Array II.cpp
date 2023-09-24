d2241f7ab4522b022bb50b8baddae83e952cc478
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-09-24 19:15:13 +0530

    Create Search In A Rotated Sorted Array II.cpp

---
+++ Search In A Rotated Sorted Array II.cpp
@@ -0,0 +1,27 @@
+bool searchInARotatedSortedArrayII(vector<int>&A, int key) {
+    // Write your code here.'
+    int low=0,high=A.size()-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(A[mid]==key){
+            return true;
+        }
+        else if(A[mid]<=A[high]){
+            if(key>A[mid] && key<=A[high]){
+                low=mid+1;
+            }
+            else{
+                high=mid-1;
+            }
+        }
+        else{
+            if(key>=A[low] && key<A[mid]){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
+        }
+    }
+    return false;
+}
