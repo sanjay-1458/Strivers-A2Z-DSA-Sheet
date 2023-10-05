5c321aea6479c184e02b31b7951be17d46e74489
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-05 19:03:11 +0530

    Update Capacity To Ship Packages Within D Days.cpp

--- Capacity To Ship Packages Within D Days.cpp
+++ Capacity To Ship Packages Within D Days.cpp
@@ -1,29 +1,31 @@
-int f(int mid,vector<int>&arr){
-    int sum=0,cnt=1;
-    for(int i=0;i<arr.size();++i){
-        sum+=arr[i];
-        if(sum>mid){
+int fun(vector<int>&arr, int w){
+    int sum=arr[0],cnt=1;
+    for(int i=1;i<arr.size();++i){
+        if(sum+arr[i]>w){
             cnt++;
             sum=arr[i];
+        }
+        else{
+            sum+=arr[i];
         }
     }
     return cnt;
 }
 
-int leastWeightCapacity(vector<int> &weights, int d)
+int leastWeightCapacity(vector<int> &arr, int d)
 {
-    // sort(weights.begin(),weights.end());
-    int maxi=weights[0],sum=0;
-    for(int i=0;i<weights.size();++i){
-        if(weights[i]>maxi){
-            maxi=weights[i];
+    // Write your code here.
+    int maxi=arr[0],sum=0;
+    for(int i=0;i<arr.size();++i){
+        if(arr[i]>maxi){
+            maxi=arr[i];
         }
-        sum+=weights[i];
+        sum+=arr[i];
     }
     int low=maxi,high=sum,ans=0;
     while(low<=high){
-        int mid=low+(high-low)/2;
-        int day=f(mid,weights);
+        int mid=low+(high-low)/2;//capacity
+        int day=fun(arr,mid);
         if(day<=d){
             ans=mid;
             high=mid-1;
