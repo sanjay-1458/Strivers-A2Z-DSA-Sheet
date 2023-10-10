8b07fb4b4525ff7a1aa8d38b5d3840a89dd95cad
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-10 20:23:37 +0530

    Update Implement Upper Bound.cpp

--- Implement Upper Bound.cpp
+++ Implement Upper Bound.cpp
@@ -2,18 +2,16 @@
 // Upper Bound = arr[i] > x;
 
 int upperBound(vector<int> &arr, int x, int n){
-	// Write your code here.	
+	// Write your code here.
 	int low=0,high=n-1;
-	int ans=n;
 	while(low<=high){
 		int mid=low+(high-low)/2;
 		if(arr[mid]<=x){
 			low=mid+1;
 		}
 		else{
-			ans=mid;
 			high=mid-1;
 		}
 	}
-	return ans;
+	return low;
 }
