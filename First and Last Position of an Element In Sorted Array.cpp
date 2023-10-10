c16d823eac8795be85b9b5683fff5f55d4c73898
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-10 22:51:07 +0530

    Update First and Last Position of an Element In Sorted Array.cpp

--- First and Last Position of an Element In Sorted Array.cpp
+++ First and Last Position of an Element In Sorted Array.cpp
@@ -1,45 +1,44 @@
-int first(vector<int>& arr,int n,int k){
-    int ans=-1;
-    int low=0,high=n-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        if(arr[mid]==k){
-            ans=mid;
-            high=mid-1;
+class Solution {
+    int p1(vector<int>&nums,int k){
+        int ans=-1;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==k){
+                ans=mid;
+                high=mid-1;
+            }
+            else if(nums[mid]>k){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        else if(arr[mid]<k){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
+        return ans;
     }
-    return ans;
-}
-
-int last(vector<int>& arr,int n,int k){
-    int ans=-1;
-    int low=0,high=n-1;
-    while(low<=high){
-        int mid=low+(high-low)/2;
-        if(arr[mid]==k){
-            ans=mid;
-            low=mid+1;
+    int p2(vector<int>&nums,int k){
+        int ans=-1;
+        int low=0,high=nums.size()-1;
+        while(low<=high){
+            int mid=low+(high-low)/2;
+            if(nums[mid]==k){
+                ans=mid;
+                low=mid+1;
+            }
+            else if(nums[mid]>k){
+                high=mid-1;
+            }
+            else{
+                low=mid+1;
+            }
         }
-        else if(arr[mid]<k){
-            low=mid+1;
-        }
-        else{
-            high=mid-1;
-        }
+        return ans;
     }
-    return ans;
-}
-
-pair<int, int> firstAndLastPosition(vector<int>& arr, int n, int k)
-{
-    int ans1=first(arr,n,k);
-    int ans2=last(arr,n,k);
-    pair<int,int> ans=make_pair(ans1,ans2);
-    return ans;
-}
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        int x=p1(nums,target);
+        int y=p2(nums,target);
+        return {x,y};
+    }
+};
