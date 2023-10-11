9fdb9a862ec0e3b459334e4dcc353c3937c0ac4e
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-11 19:27:23 +0530

    Update Find Nth Root Of M.cpp

--- Find Nth Root Of M.cpp
+++ Find Nth Root Of M.cpp
@@ -1,31 +1,30 @@
-long long pow(long long a,long long b){
-  while(b>0){
-    long long ans=pow(a,b/2);
-    ans=ans*ans;
-    if(b%2){
-      return ans;
+int fun(int mid,int n){
+  long long ans=1;
+  for(int i=0;i<n;++i){
+    if(ans>INT_MAX/mid){
+      return INT_MAX;
     }
-    else{
-      return ans*a;
-    }
+    ans*=mid;
   }
+  return ans;
 }
 
+
 int NthRoot(int n, int m) {
-  // Write your code here.
-  long long ans=-1,low=1,high=m;
+  int low=1,high=m;
+  int ans=-1;
   while(low<=high){
-    long long mid=low+(high-low)/2;
-    if(pow(mid,n)==m){
-      ans=mid;
-      break;
+    int mid=low+(high-low)/2;
+    int val=fun(mid,n);
+    if(val==m){
+      return mid;
     }
-    else if(pow(mid,n)>m){
+    else if(val>m){
       high=mid-1;
     }
     else{
       low=mid+1;
     }
   }
-  return ans;
+  return -1;
 }
