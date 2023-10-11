134326209d5d395e0c8f571c321f15ff827d6f8f
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-10-11 17:11:01 +0530

    Update Find out how many times the array has been rotated.cpp

--- Find out how many times the array has been rotated.cpp
+++ Find out how many times the array has been rotated.cpp
@@ -29,3 +29,28 @@ int findKRotation(vector<int> &arr){
     }
     return ans;
 }
+
+
+int findKRotation(vector<int> &arr){
+    int ans=arr[0];
+    int ind=0;
+    int low=0,high=arr.size()-1;
+    while(low<=high){
+        int mid=low+(high-low)/2;
+        if(arr[low]<=arr[mid]){
+            if(arr[low]<ans){
+                ans=arr[low];
+                ind=low;
+            }
+            low=mid+1;
+        }
+        else{
+            if(arr[mid]<ans){
+                ans=arr[mid];
+                ind=mid;
+            }
+            high=mid-1;
+        }
+    }
+    return ind;
+}
