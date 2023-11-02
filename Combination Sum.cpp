69926f50f905d3f46310b4cb4bfe78513fb12480
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-11-02 19:16:36 +0530

    Update Combination Sum.cpp

--- Combination Sum.cpp
+++ Combination Sum.cpp
@@ -17,6 +17,49 @@ void fun(int ind,int sum,vector<int> vec,vector<vector<int>>&res,vector<int>&arr
 }
 
 
+void fun(int ind,int sum,vector<int> vec,vector<vector<int>>&res,vector<int>&arr,int k){
+    if(sum==k){
+        sort(vec.begin(),vec.end());
+        res.push_back(vec);
+        return;
+    }
+    if(sum>k){
+        return;
+    }
+    if(ind>=arr.size()){
+        return;
+    }
+    if(arr[ind]<=k){
+        fun(ind+1,sum,vec,res,arr,k);
+        sum+=arr[ind];
+        vec.push_back(arr[ind]);
+        fun(ind,sum,vec,res,arr,k);
+    }
+    else{
+        fun(ind+1,sum,vec,res,arr,k);
+    }
+}
+
+void fun(int ind,int sum,vector<int> vec,vector<vector<int>>&res,vector<int>&arr,int k){
+    if(sum==k){
+        sort(vec.begin(),vec.end());
+        res.push_back(vec);
+        return;
+    }
+    if(sum>k){
+        return;
+    }
+    if(ind>=arr.size()){
+        return;
+    }
+    fun(ind+1,sum,vec,res,arr,k);
+    sum+=arr[ind];
+    vec.push_back(arr[ind]);
+    fun(ind,sum,vec,res,arr,k);
+}
+
+
+
 vector<vector<int>> combSum(vector<int> &arr, int k)
 {
     vector<vector<int>> res;
