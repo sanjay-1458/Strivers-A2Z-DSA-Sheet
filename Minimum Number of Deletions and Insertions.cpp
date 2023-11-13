e8c5226f1f2d0a4cdfa52a2e1883d4954aa72ddb
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-11-13 19:08:16 +0530

    Update Minimum Number of Deletions and Insertions.cpp

--- Minimum Number of Deletions and Insertions.cpp
+++ Minimum Number of Deletions and Insertions.cpp
@@ -24,7 +24,7 @@ int canYouMake1(string &s1, string &s2){
     vector<vector<int>> dp(n+1,vector<int>(m+1,-1));
     return fun(n,m,s1,s2,dp);
 }
-int canYouMake(string &s1, string &s2){
+int canYouMake2(string &s1, string &s2){
     int n=s1.size(),m=s2.size();
     vector<vector<int>> dp(n+1,vector<int>(m+1,0));
     for(int i=1;i<=n;++i){
@@ -45,4 +45,27 @@ int canYouMake(string &s1, string &s2){
         }
     }
     return dp[n][m];
+}
+int canYouMake(string &s1, string &s2){
+    int n=s1.size(),m=s2.size();
+    vector<vector<int>> dp(n+1,vector<int>(m+1,0));
+    for(int i=0;i<=n;++i){
+        dp[i][0]=0;
+    }
+    for(int j=0;j<=m;++j){
+        dp[0][j]=0;
+    }
+    for(int i=1;i<=n;++i){
+        for(int j=1;j<=m;++j){
+            if(s1[i-1]==s2[j-1]){
+                dp[i][j]=1+dp[i-1][j-1];
+            }
+            else{
+                dp[i][j]=max(dp[i-1][j],dp[i][j-1]);
+            }
+        }
+    }
+    int d1=n-dp[n][m];
+    int d2=m-dp[n][m];
+    return d1+d2;
 }
