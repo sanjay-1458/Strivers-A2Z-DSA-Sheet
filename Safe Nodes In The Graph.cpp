68c32113e32d4270abf4ab678f505a5d753f816f
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-11-16 11:34:20 +0530

    Update Safe Nodes In The Graph.cpp

--- Safe Nodes In The Graph.cpp
+++ Safe Nodes In The Graph.cpp
@@ -1,34 +1,42 @@
 #include <bits/stdc++.h> 
 
-bool dfs(int node,vector<int> adj[],vector<int>&vis,vector<int> pathvis){
+bool dfs(int node,vector<int> adj[],vector<int> &pathvis,vector<int>&vis,vector<int>&check){
     vis[node]=1;
     pathvis[node]=1;
+    check[node]=1;
     for(auto x:adj[node]){
         if(!vis[x]){
-            if(dfs(x,adj,vis,pathvis)==true){
+            if(dfs(x,adj,pathvis,vis,check)==true){
+                check[node]=0;
                 return true;
             }
         }
         else if(pathvis[x]){
+            check[node]=0;
             return true;
         }
     }
+    check[node]=1;
+    pathvis[node]=0;
     return false;
 }
 
-vector<int> safeNodes(vector<vector<int>> &edges, int n, int e) {
-    vector<int> adj[n];
+vector<int> safeNodes(vector<vector<int>> &edges, int v, int e) {
+	vector<int> adj[v];
     for(int i=0;i<e;++i){
         adj[edges[i][0]].push_back(edges[i][1]);
     }
+    vector<int> vis(v,0);
+    vector<int> pathvis(v,0);
+    vector<int> check(v,0);
     vector<int> ans;
-    for(int i=0;i<n;++i){
-        vector<int> vis(n,0);
-        vector<int> pathvis(n,0);
-        if(dfs(i,adj,vis,pathvis)==true){
-            continue;
+    for(int i=0;i<v;++i){
+        if(!vis[i]){
+            dfs(i,adj,pathvis,vis,check);
         }
-        else{
+    }
+    for(int i=0;i<v;++i){
+        if(check[i]==1){
             ans.push_back(i);
         }
     }
