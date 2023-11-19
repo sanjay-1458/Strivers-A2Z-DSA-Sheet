70429a5a0e277cc1272787716829ca1744fd2850
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-11-19 20:18:26 +0530

    Update Insertion Sort.cpp

--- Insertion Sort.cpp
+++ Insertion Sort.cpp
@@ -26,3 +26,19 @@ void insertionSort(int arr[], int n)
         arr[j+1]=key;
     }
 }
+
+
+#include<bits/stdc++.h>
+void insertionSort(int arr[], int n)
+{
+    for(int i=1;i<n;++i){
+        int key=arr[i];
+        int j=i;
+        while(j>0 && arr[j]<arr[j-1]){
+            int temp=arr[j];
+            arr[j]=arr[j-1];
+            arr[j-1]=temp;
+            j--;
+        }
+    }
+}
