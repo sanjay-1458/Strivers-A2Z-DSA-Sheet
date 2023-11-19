f12f330f99c6530b1e21ecc247c05ecfa0e1035d
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2023-11-19 20:00:39 +0530

    Update Selection Sort.cpp

--- Selection Sort.cpp
+++ Selection Sort.cpp
@@ -4,12 +4,13 @@ void selectionSort(vector<int>&arr) {
     int i=0,n=arr.size();
     for(i=0;i<n-1;++i){
         int mini=INT_MAX;int ind=i;
-        for(int j=i;j<n;++j){
+        for(int j=i+1;j<n;++j){
             if(mini>arr[j]){
                 mini=arr[j];
                 ind=j;
             }
         }
-        swap(arr[i],arr[ind]);
+        if(i!=ind)
+            swap(arr[i],arr[ind]);
     }
 }
