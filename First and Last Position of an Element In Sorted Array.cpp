b951c314c771ff881c7f120c048985398845ccdc
Sanjay <121040100+sanjay-1458@users.noreply.github.com>
2024-03-20 22:21:56 +0530

    Update First and Last Position of an Element In Sorted Array.cpp

--- First and Last Position of an Element In Sorted Array.cpp
+++ First and Last Position of an Element In Sorted Array.cpp
@@ -42,3 +42,49 @@ public:
         return {x,y};
     }
 };
+class Solution {
+    int f(vector<int>&a,int x){
+        int l=0,h=(int)a.size()-1;
+        int ans=-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(a[m]==x){
+                ans=m;
+                h=m-1;
+            }
+            else if(a[m]<x){
+                l=m+1;
+            }
+            else if(a[m]>x){
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+
+    int lt(vector<int>&a,int x){
+        int l=0,h=(int)a.size()-1;
+        int ans=-1;
+        while(l<=h){
+            int m=(l+h)/2;
+            if(a[m]==x){
+                ans=m;
+                l=m+1;
+            }
+            else if(a[m]<x){
+                l=m+1;
+            }
+            else if(a[m]>x){
+                h=m-1;
+            }
+        }
+        return ans;
+    }
+public:
+    vector<int> searchRange(vector<int>& nums, int t) {
+        int fr=f(nums,t);
+        int ls=lt(nums,t);
+        if(fr==-1) return {-1,-1};
+        else return {fr,ls};
+    }
+};
